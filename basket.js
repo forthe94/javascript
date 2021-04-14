59a7715d1eb3b5cfadc62077bb8df9dae3973929
Александр Тетенков <tetenkov@tecon.ru>
2021-04-14 15:24:56 +0300

    Сделал корзину

---
+++ basket.js
@@ -0,0 +1,22 @@
+"use strict"
+
+function countBasketPrice(basket)
+{
+    let total = 0;
+    var good = 0;
+    while (good < basket.length)
+    {
+	total += basket[good][1]
+	good++;
+    }
+    return total;
+}
+
+
+var basket = [];
+
+basket[0] = ['Творог', 50];
+basket[1] = ['Сметан', 54];
+basket[2] = ['Колбаса', 100];
+basket[3] = ['Молоко', 70];
+console.log(countBasketPrice(basket));
